01609eb0dcccbd6800559c09035bd2577b26bcc5
sheriff <chege2452@gmail.com>
2024-08-21 13:47:17 +0300

    i comleted the task

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,32 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS); 
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const categories = CATEGORIES;
+
+  const handleDeleteTask = (taskText) => {
+    setTasks(tasks.filter((task) => task.text !== taskText));
+  };
+
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const visibleTasks = tasks.filter((task) => {
+    if (selectedCategory === "All") return true;
+    return task.category === selectedCategory;
+  });
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={categories}
+        selectedCategory={selectedCategory}
+        onSelectCategory={setSelectedCategory}/>
+      <NewTaskForm categories={categories.filter((cat) => cat !== "All")} onTaskFormSubmit={handleAddTask} />
+      <TaskList tasks={visibleTasks} onDeleteTask={handleDeleteTask}/>
     </div>
   );
 }
